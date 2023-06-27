49efa068c41c3e7c37f6f12f86a6e54e3b4db6ca
Varinder Singh <48336937+varindersingh1995@users.noreply.github.com>
2023-06-27 16:57:14 -0400

    assets folder

---
+++ assets/js/CRUDVideo.js
@@ -0,0 +1,17 @@
+player = document.getElementById("playButton");
+player.addEventListener('click', VideoPlay);
+
+function VideoPlay() {
+	document.getElementById("playerdiv").style.display = "block";
+	document.getElementById("player").src="media/CRUDdemo.mp4";
+	document.getElementById("player").play();
+	document.getElementById("close").style.display="block";
+  document.getElementById("playButton").style.display="none";
+}	
+
+document.getElementById("close").addEventListener('click',closePlayer);
+function closePlayer(){
+  document.getElementById("playerdiv").style.display = "none";
+  document.getElementById("close").style.display="none";
+  document.getElementById("playButton").style.display="block";
+}
